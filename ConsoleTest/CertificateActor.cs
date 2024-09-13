2b064b8894e8f06eb0e6b2a54d862fab465e75e4
Dennis.Nguyen <dennis.nguyen@wisetechglobal.com>
2024-09-13 14:52:22 +1000

    Add project files.

---
+++ ConsoleTest/CertificateActor.cs
@@ -0,0 +1,85 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ConsoleTest;
+
+/// <summary>
+/// Based on example from: https://learn.microsoft.com/en-us/dotnet/api/system.security.cryptography.x509certificates.x509certificate2?view=net-8.0
+/// </summary>
+public class CertificateActor: IActor
+{
+	/// <summary>
+	/// Create X509Certificate2 object from .cer file.
+	/// </summary>
+	public CertificateActor(string dataFilePath) 
+	{
+		var rawData = File.ReadAllBytes(dataFilePath);
+		X509 = new X509Certificate2(rawData);
+	}
+
+	public CertificateActor(byte[] rawData)
+	{
+		X509 = new X509Certificate2(rawData);
+	}
+
+	X509Certificate2 X509 { get; set; }
+
+	RSA RsaPublicKey { 
+		get
+		{
+			var publicKey = X509.GetRSAPublicKey();
+			if (publicKey == null)
+			{
+				throw new SystemException("Certificate missing RSA public key");
+			}
+
+			return publicKey;
+		} 
+	}
+
+	public void PrintInformationToConsole()
+	{
+		Console.WriteLine("Subject: {0}.", X509.Subject);
+		Console.WriteLine("Issuer: {0}.", X509.Issuer);
+		Console.WriteLine("Version: {0}.", X509.Version);
+		Console.WriteLine("Valid Date: {0}.", X509.NotBefore);
+		Console.WriteLine("Expiry Date: {0}.", X509.NotAfter);
+		Console.WriteLine("Thumbprint: {0}.", X509.Thumbprint);
+		Console.WriteLine("Serial Number: {0}.", X509.SerialNumber);
+		Console.WriteLine("Friendly Name: {0}.", X509.PublicKey.Oid.FriendlyName);
+		Console.WriteLine("Public Key Format: {0}.", X509.PublicKey.EncodedKeyValue.Format(true));
+		Console.WriteLine("Raw Data Length: {0}.", X509.RawData.Length);
+		Console.WriteLine("Certificate to string: {0}.", X509.ToString(true));
+		Console.WriteLine("Certificate to XML String: {0}.", X509.GetRSAPublicKey()?.ToXmlString(false));
+	}
+
+	public byte[] EncryptData(byte[] data)
+	{
+		return RsaPublicKey.Encrypt(data, RSAEncryptionPadding.Pkcs1);
+	}
+
+	public byte[] DecryptData(byte[] data)
+	{
+		return RsaPublicKey.Decrypt(data, RSAEncryptionPadding.Pkcs1);
+	}
+
+	public byte[] SignData(byte[] data)
+	{
+		return RsaPublicKey.SignData(data, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+	}
+
+	public bool VerifySignature(byte[] data, byte[] signature)
+	{
+		return RsaPublicKey.VerifyData(data, signature, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+	}
+
+	public static byte[] ReadFile(string fileName)
+	{
+		using var fileStream = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+		int size = (int)fileStream.Length;
+		byte[] data = new byte[size];
+		size = fileStream.Read(data, 0, size);
+		return data;
+	}
+}
---
+++ ConsoleTest/Program.cs
@@ -0,0 +1,88 @@
+﻿using System.Text;
+
+namespace ConsoleTest
+{
+	internal class Program
+	{
+		static readonly string BaseDir = @"C:\WTG\IL\WI00793348_CargoVisibilityAPI_SetupS2ST\01_CertificateSigningRequest";
+
+		static readonly string CertificateFile = $"{BaseDir}/WTG,ILS,CargoVisibilityAPI - f30a1049-9790-4653-bbac-79c7a040278c.cer";
+		static readonly string PublicKeyFile = $"{BaseDir}/public_key.pem";
+		static readonly string PrivateKeyFile = $"{BaseDir}/private.key";
+
+		static readonly string TestDataFile = $"{BaseDir}/TestData_ZIMUFLX09069253.json";
+
+		static void Main(string[] args)
+		{
+			if (args.Length < 1)
+			{
+				Console.WriteLine("Missing arguments: <ProgramName> <Command> <CommandArguments...>");
+				return;
+			}
+
+			Console.WriteLine("Arguments: {0}", string.Join(',', args));
+			switch (args[0].ToLowerInvariant())
+			{
+				case "print_certificate":
+					PrintCertificate(CertificateFile);
+					return;
+				case "print_public_pem":
+					PrintRsaKey(PublicKeyFile);
+					return;
+				case "print_private_pem":
+					PrintRsaKey(PrivateKeyFile);
+					return;
+				case "print_all":
+					var lineSep = new string('-', 20);
+
+					Console.WriteLine(lineSep);
+					PrintCertificate(CertificateFile);
+
+					Console.WriteLine(lineSep);
+					PrintRsaKey(PublicKeyFile);
+
+					Console.WriteLine(lineSep);
+					PrintRsaKey(PrivateKeyFile);
+					return;
+				case "encrypt_by_cert":
+					{
+						var certificateActor = new CertificateActor(CertificateFile);
+						var privateKeyActor = new RsaKeyActor(PrivateKeyFile);
+
+						var rawData = File.ReadAllBytes(TestDataFile);
+						var encryptedData = certificateActor.EncryptData(rawData);
+						var decryptedData = privateKeyActor.DecryptData(encryptedData);
+						Console.WriteLine(Encoding.UTF8.GetString(decryptedData));
+					}
+					return;
+				case "sign_by_private":
+					{
+						var certificateActor = new CertificateActor(CertificateFile);
+						var privateKeyActor = new RsaKeyActor(PrivateKeyFile);
+
+						var rawData = File.ReadAllBytes(TestDataFile);
+						var signature = privateKeyActor.SignData(rawData);
+						var verifyResult = certificateActor.VerifySignature(rawData, signature);
+
+						Console.WriteLine("Verify result: {0} with signature {1}", verifyResult, Convert.ToBase64String(signature));
+					}
+					return;
+				default:
+					Console.WriteLine($"Invalid command: {args[0]}");
+					return;
+			}
+		}
+
+		static void PrintCertificate(string filePath)
+		{
+			var certificateActor = new CertificateActor(filePath);
+			certificateActor.PrintInformationToConsole();
+		}
+
+		static void PrintRsaKey(string filePath)
+		{
+			var rsaKeyActor = new RsaKeyActor(filePath);
+			rsaKeyActor.PrintInformationToConsole();
+		}
+	}
+}
